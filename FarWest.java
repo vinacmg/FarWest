21ddda55b93d3deaacf2d7393f6c3f0735df08e3
vinacmg <viniciuscmgo@gmail.com>
2017-06-22 15:08:58 -0300

    addded FarWest.java

---
+++ FarWest.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
+import javax.imageio.*;
+
+class FarWest extends JFrame {
+
+	static public void main(String[] args){
+		System.out.println("FarWest!!!");
+	} 
+}
