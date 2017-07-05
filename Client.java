519ed8032f58bbe72aba1975fd87f7add30ebad5
vinacmg <viniciuscmgo@gmail.com>
2017-07-05 19:24:15 -0300

    teste

--- Client.java
+++ Client.java
@@ -12,8 +12,11 @@ class Client extends JFrame {
 	final int STANDING1 = 1;
 	final int CROUCHING = 2;
 	int estado = STANDING1;
+	int height;
+	Desenho tela = new Desenho();
 
 	class Desenho extends JPanel{
+
 
 		Desenho(){
 			img = new Image[3];
@@ -30,25 +33,55 @@ class Client extends JFrame {
 		public void paintComponent(Graphics g) {
 	    	super.paintComponent(g);
 	    	g.drawImage(img[BACKGROUND], 0, 0, getSize().width, getSize().height, this);
-	    	g.drawImage(img[STANDING1], getSize().width - img[STANDING1].getWidth(this) - 10, getSize().height - img[STANDING1].getHeight(this) - 20, img[STANDING1].getWidth(this), img[STANDING1].getHeight(this), this);
+	    	g.drawImage(img[STANDING1], getSize().width - img[STANDING1].getWidth(this) - 10,
+	    		getSize().height - img[STANDING1].getHeight(this) - height, 
+	    		(int)Math.ceil((getSize().width/1200.0)*img[STANDING1].getWidth(this)), 
+	    		(int)Math.ceil((getSize().height/699.0)*img[STANDING1].getHeight(this)), this);
     	}
 		
 		public Dimension getPreferredSize() {
-      		return new Dimension(1200, 700);
+      		return new Dimension(1200, 699);
     	}
 
-	}
+    }
 
+	public class Pular extends Thread{
+		int y = height;
+		final int MAX = img[STANDING1].getHeight(tela);
+		public void run(){
+			while(height < MAX){
+				height++;
+				repaint();
+				try{
+					sleep(2);
+				} catch (InterruptedException e) {};
+			}
+			while(height > y){
+				height--;
+				repaint();
+				try{
+					sleep(2);
+				} catch (InterruptedException e) {};
+			}
+		}
+	}
 
 	Client() {
 	    super("FarWest");
 	    setDefaultCloseOperation(EXIT_ON_CLOSE);
-	    add(new Desenho());
+	    add(tela);
 	    pack();
 	    setVisible(true);
+	    height = (int)Math.ceil(tela.getSize().height/37.0);
+	    while(true){
+		    try{
+		    	new Pular().start();
+				Thread.sleep(1500);
+			} catch (InterruptedException e) {};
+		}
   	}
 
 	public static void main(String[] args) {
-		new Client();
+		Client c = new Client();
 	}
 }
