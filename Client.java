9619e45b61736ff02a55f7ba86492d1d527fdae2
vinacmg <viniciuscmgo@gmail.com>
2017-07-04 21:15:20 -0300

    printando ambiente

--- Client.java
+++ Client.java
@@ -18,7 +18,7 @@ class Client extends JFrame {
 		Desenho(){
 			img = new Image[3];
 			try {
-				img[BACKGROUND] = ImageIO.read(new File("backgroung.png"));
+				img[BACKGROUND] = ImageIO.read(new File("background.png"));
 				img[STANDING1] = ImageIO.read(new File("standing1.png"));
 				img[CROUCHING] = ImageIO.read(new File("agachado1.png"));
 	      	} catch (IOException e) {
@@ -30,10 +30,25 @@ class Client extends JFrame {
 		public void paintComponent(Graphics g) {
 	    	super.paintComponent(g);
 	    	g.drawImage(img[BACKGROUND], 0, 0, getSize().width, getSize().height, this);
+	    	g.drawImage(img[STANDING1], getSize().width - img[STANDING1].getWidth(this) - 10, getSize().height - img[STANDING1].getHeight(this) - 20, img[STANDING1].getWidth(this), img[STANDING1].getHeight(this), this);
     	}
+		
+		public Dimension getPreferredSize() {
+      		return new Dimension(1200, 700);
+    	}
+
 	}
 
+
+	Client() {
+	    super("FarWest");
+	    setDefaultCloseOperation(EXIT_ON_CLOSE);
+	    add(new Desenho());
+	    pack();
+	    setVisible(true);
+  	}
+
 	public static void main(String[] args) {
-		
+		new Client();
 	}
 }
